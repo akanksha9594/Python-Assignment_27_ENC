c672bab14c05e474ac1408b6b42235d81fc2e17e
akanksha9594 <121791054+akanksha9594@users.noreply.github.com>
2023-01-02 13:54:39 +0530

    Add files via upload

---
+++ PYTHON ASSIGNMENT ENC 27 (1).py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  # ASSIGNMENT 1 SUBMITTED BY AKANKSHA MISHRA
+
+# ### WRITE A PYTHON PROGRAMME TO CALCULATE THE LENGTH OF A STRING
+
+# # 
+
+# In[4]:
+
+
+length= len(input('write a sentence:  '))
+print (length)
+
+
+# ### python programme to calculate the square root
+
+# In[5]:
+
+
+import math
+number=float(input("enter a number:"))
+sqrt= math.sqrt(number)
+print(f"the square root of{number}is {sqrt}")
+
+
+# ### write a python programme to convert temperature in celciusto temperature in farehneit
+
+# In[26]:
+
+
+celsius=25
+Fahrenheit = celsius*9/5+32
+print(Fahrenheit)
+
+
+# In[25]:
+
+
+data=(10,4,5,2+6j)
+
+
+# In[22]:
+
+
+a=10
+print (type (a))
+
+
+# In[15]:
+
+
+b=4.5
+print(type(b))
+
+
+# In[17]:
+
+
+c=2+6j
+print(type(c))
+
+
+# In[ ]:
+
+
+
+
